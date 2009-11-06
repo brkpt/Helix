c7e3570a7427fb23f903b1f01141cffce25b35b9
Matt Smith <boulderfans@comcast.net>
2009-11-06 13:15:13 -0700

    cleanup

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,43 +8,6 @@
 #include "Utility/Memory/FixedAlloc.h"
 #include "Utility/Container/Array.h"
 
-class Foo
-{
-public:
-	Foo()
-	{
-		index = ++totalCount;
-	}
-
-	Foo(const Foo &other)
-	{
-		index = ++totalCount;
-	}
-
-	~Foo()
-	{
-		index = 0;
-	}
-
-	const Foo &operator=(const Foo &other)
-	{
-		index = ++totalCount;
-		return *this;
-	}
-	DECLARE_FIXED_ALLOC(Foo);
-
-private:
-	char	fooData[100];
-	int		index;
-
-	static int	totalCount;
-};
-
-IMPLEMENT_FIXED_ALLOC(Foo,5);
-
-int Foo::totalCount = 0;
-
-
 // ****************************************************************************
 // ****************************************************************************
 int APIENTRY _tWinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPTSTR lpCmdLine,int nCmdShow)
