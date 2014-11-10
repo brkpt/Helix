71afc11a9c710fd79e2d69d448b92229a3bf879c
Matt Smith <boulderfans@comcast.net>
2014-11-10 10:08:46 -0700

    Get rid of the template version and add Vector4

--- src/Helix/Math/Vector.cpp
+++ src/Helix/Math/Vector.cpp
@@ -1,3 +1,262 @@
 #include "Vector.h"
 
+// ****************************************************************************
+// ****************************************************************************
+Helix::Vector3 operator*(const float lhs, const Helix::Vector3 &rhs)
+{
+	Helix::Vector3 result = rhs*lhs;
+	return result;
+}
 
+Helix::Vector3 operator/(const float lhs, const Helix::Vector3 &rhs)
+{
+	Helix::Vector3 result = rhs*lhs;
+	return result;
+}
+
+// ****************************************************************************
+// ****************************************************************************
+Helix::Vector4 operator*(const float lhs, const Helix::Vector4 &rhs)
+{
+	Helix::Vector4 result = rhs*lhs;
+	return result;
+}
+
+Helix::Vector4 operator/(const float lhs, const Helix::Vector4 &rhs)
+{
+	Helix::Vector4 result = rhs*lhs;
+	return result;
+}
+
+
+namespace Helix {
+
+// ****************************************************************************
+// ****************************************************************************
+Vector3::Vector3()
+	: x(0.f)
+	, y(0.f)
+	, z(0.f)
+{}
+
+Vector3::Vector3(const float _x, const float _y, const float _z)
+	: x(_x)
+	, y(_y)
+	, z(_z)
+{}
+
+Vector3::Vector3(const Vector3 &other)
+{
+	x = other.x;
+	y = other.y;
+	z = other.z;
+}
+
+Vector3::Vector3(const float *in)
+{
+	x = in[0];
+	y = in[1];
+	z = in[2];
+}
+
+Vector3 Vector3::Cross(const Vector3 &other) const
+{
+	Vector3 result;
+	result.x = y*other.z - z*other.y;
+	result.y = z*other.x - x*other.z;
+	result.z = x*other.y - y*other.z;
+	return result;
+}
+
+Vector3 Vector3::operator+(const Vector3 &other) const
+{
+	Vector3 result;
+
+	result.x = x + other.x;
+	result.y = y + other.y;
+	result.z = z + other.z;
+
+	return result;
+}
+
+Vector3 Vector3::operator-(const Vector3 &other) const
+{
+	Vector3 result;
+	result.x = x - other.x;
+	result.y = y - other.y;
+	result.z = z - other.z;
+	return result;
+}
+
+Vector3 Vector3::operator*(const float scalar) const
+{
+	Vector3 result;
+	result.x = x * scalar;
+	result.y = y * scalar;
+	result.z = z * scalar;
+	return result;
+}
+
+Vector3 Vector3::operator/(const float scalar) const
+{
+	Vector3 result;
+	result.x = x / scalar;
+	result.y = y / scalar;
+	result.z = z / scalar;
+	return result;
+}
+
+Vector3 & Vector3::operator+=(const Vector3 &other)
+{
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	return *this;
+}
+
+Vector3 & Vector3::operator-=(const Vector3 &other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	return *this;
+}
+
+Vector3 & Vector3::operator*=(const float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+	return *this;
+}
+
+Vector3 & Vector3::operator/=(const float scalar)
+{
+	x /= scalar;
+	y /= scalar;
+	z /= scalar;
+	return *this;
+}
+
+float Vector3::Dot(const Vector3 &v2) const
+{
+	float retVal = x*v2.x + y*v2.y + z*v2.z;
+	return retVal;
+}
+
+float Vector3::Dot(const Vector3 &v1, const Vector3 &v2)
+{
+	return v1.Dot(v2);
+}
+
+// ****************************************************************************
+// ****************************************************************************
+Vector4::Vector4()
+	: x(0.f)
+	, y(0.f)
+	, z(0.f)
+	, w(0.f)
+{}
+
+Vector4::Vector4(const float _x, const float _y, const float _z, const float _w)
+	: x(_x)
+	, y(_y)
+	, z(_z)
+	, w(_w)
+{}
+
+Vector4::Vector4(const Vector4 &other)
+{
+	x = other.x;
+	y = other.y;
+	z = other.z;
+	w = other.w;
+}
+
+Vector4::Vector4(const float *in)
+{
+	x = in[0];
+	y = in[1];
+	z = in[2];
+	w = in[3];
+}
+
+Vector4 Vector4::operator+(const Vector4 &other) const
+{
+	Vector4 result;
+
+	result.x = x + other.x;
+	result.y = y + other.y;
+	result.z = z + other.z;
+	result.w = z + other.w;
+
+	return result;
+}
+
+Vector4 Vector4::operator-(const Vector4 &other) const
+{
+	Vector4 result;
+	result.x = x - other.x;
+	result.y = y - other.y;
+	result.z = z - other.z;
+	result.w = w - other.w;
+	return result;
+}
+
+Vector4 Vector4::operator*(const float scalar) const
+{
+	Vector4 result;
+	result.x = x * scalar;
+	result.y = y * scalar;
+	result.z = z * scalar;
+	result.w = w * scalar;
+	return result;
+}
+
+Vector4 Vector4::operator/(const float scalar) const
+{
+	Vector4 result;
+	result.x = x / scalar;
+	result.y = y / scalar;
+	result.z = z / scalar;
+	result.w = z / scalar;
+	return result;
+}
+
+Vector4 & Vector4::operator+=(const Vector4 &other)
+{
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	w += other.w;
+	return *this;
+}
+
+Vector4 & Vector4::operator-=(const Vector4 &other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	w -= other.w;
+	return *this;
+}
+
+Vector4 & Vector4::operator*=(const float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+	w *= scalar;
+	return *this;
+}
+
+Vector4 & Vector4::operator/=(const float scalar)
+{
+	x /= scalar;
+	y /= scalar;
+	z /= scalar;
+	w /= scalar;
+	return *this;
+}
+
+} // namespace Helix
--- src/Helix/Math/Vector.h
+++ src/Helix/Math/Vector.h
@@ -3,153 +3,58 @@
 
 namespace Helix
 {
-// Generic form
-// VT = underlying type
-// D = dimensions
-template <typename VT, int D>
-struct Vector
+struct Vector3
 {
-	Vector();
-	Vector(const Vector<VT, D> &other);
-	Vector(VT init[D]);
-	Vector<VT, D>	operator+(const Vector<VT, D> &rhs) const;
-	Vector<VT, D>	operator-(const Vector<VT, D> &rhs) const;
+	Vector3();
+	Vector3(const float x, const float y, const float z);
+	Vector3(const Vector3 &other);
+	Vector3(const float init[3]);
 
-	template <typename T1> Vector<VT,D>	operator*(const T1 scalar) const;
-	template <typename T1> Vector<VT,D>	operator/(const T1 scalar) const;
+	float			Dot(const Vector3 &v2) const;
+	static float	Dot(const Vector3 &v1, const Vector3 &v2);
+	Vector3			Cross(const Vector3 &v2) const;
 
-	void Zero();
+	Vector3		operator+(const Vector3 &rhs) const;
+	Vector3		operator-(const Vector3 &rhs) const;
+	Vector3		operator*(const float scalar) const;
+	Vector3		operator/(const float scalar) const;
+	Vector3 &	operator+=(const Vector3 &rhs);
+	Vector3 &	operator-=(const Vector3 &rhs);
+	Vector3 &	operator*=(const float scalar);
+	Vector3 &	operator/=(const float scalar);
 
-	VT data[D];
+	void Zero() { x = 0.f; y = 0.f; z = 0.f; }
+
+	// statics
+	float	x, y, z;
 };
 
-// Generic methods
-template <typename VT, int D>
-Vector<VT, D>::Vector()
+struct Vector4
 {
-	for (int i = 0; i < D; i++)
-		data[i] = (VT)0;
-}
+	Vector4();
+	Vector4(const float x, const float y, const float z, float w);
+	Vector4(const Vector4 &other);
+	Vector4(const float init[3]);
+	Vector4		operator+(const Vector4 &rhs) const;
+	Vector4		operator-(const Vector4 &rhs) const;
+	Vector4		operator*(const float scalar) const;
+	Vector4		operator/(const float scalar) const;
+	Vector4 &	operator+=(const Vector4 &rhs);
+	Vector4 &	operator-=(const Vector4 &rhs);
+	Vector4 &	operator*=(const float scalar);
+	Vector4 &	operator/=(const float scalar);
 
-template <typename VT, int D>
-Vector<VT, D>::Vector(const Vector<VT, D> &other)
-{
-	for (int i = 0; i < D; i++)
-		data[i] = other.data[i];
-}
+	void Zero() { x = 0.f; y = 0.f; z = 0.f; w = 0.f; }
 
-template <typename VT, int D>
-Vector<VT,D>::Vector(VT init[D])
-{
-	for (int i = 0; i < D; i++)
-		data[i] = init[i];
-}
-
-template <typename VT, int D>
-Vector<VT, D> Vector<VT,D>::operator+(const Vector<VT, D> &rhs) const
-{
-	Vector<VT, D> result;
-	for (int i = 0; i < D; i++)
-		result.data[i] = data[i] + rhs.data[i];
-
-	return result;
-}
-
-template <typename VT, int D>
-Vector<VT,D> Vector<VT,D>::operator-(const Vector<VT,D> &rhs) const
-{
-	Vector<VT, D> result;
-	for (int i = 0; i < D; i++)
-		result.data[i] = data[i] - rhs.data[i];
-	return result;
-}
-
-template <typename VT, int D>
-template <typename T>
-Vector<VT, D> Vector<VT, D>::operator*(const T rhs) const 
-{
-	Vector<VT, D> result;
-	for (int i = 0; i < D; i++)
-		result.data[i] = data[i] * rhs;
-
-	return result;
-}
-
-template <typename VT, int D>
-template <typename T>
-Vector<VT, D> Vector<VT, D>::operator/(const T rhs) const 
-{
-	Vector<VT, D> result;
-	for (int i = 0; i < D; i++)
-		result.data[i] = data[i] / rhs;
-
-	return result;
-}
-
-template <typename VT, int D>
-void Vector<VT, D>::Zero()
-{
-	for (int i = 0; i < D; i++)
-		data[i] = (VT)0;
-}
-
-
-// Specializations
-
-// // 2D float
-// template <>
-// struct Vector<float, 2>
-// {
-// 	union {
-// 		float x, y;
-// 		float data[2];
-// 	};
-// };
-
-// 3D float
-template <>
-struct Vector<float, 3>
-{
-	union {
-		float x, y, z;
-		float data[3];
-	};
+	float	x, y, z, w;
 };
-
-// Constructor functions
-Vector<float, 3> MakeFloatVector3(float x, float y, float z)
-{
-	Vector<float, 3> result;
-	result.data[0] = x;
-	result.data[1] = y;
-	result.data[2] = z;
-	return result;
-}
-
-// Short hand constructors
-#define Vector3(x,y,z) Helix::MakeFloatVector3(x,y,z)
-
-// Free standing operators
-
-template <typename T1, typename VT, int D>
-Vector<VT, D> operator+(const T1 scalar, const Vector<VT, D> &rhs)
-{
-	Vector<VT, D> result = rhs*scalar;
-	return result;
-}
-
-template <typename T1, typename VT, int D>
-Vector<VT, D> operator*(const T1 scalar, const Vector<VT, D> &rhs)
-{
-	// Flip the parameters and call the class version
-	Vector<VT, D> result = rhs*scalar;
-	return result;
-}
-
-// Short hand typedefs
-typedef Vector<float, 3>	Vector3;
-typedef Vector<float, 2>	Vector2;
 
 } // namespace Helix
+
+Helix::Vector3 operator*(const float lhs, const Helix::Vector3 &rhs);
+Helix::Vector3 operator/(const float lhs, const Helix::Vector3 &rhs);
+Helix::Vector4 operator*(const float lhs, const Helix::Vector4 &rhs);
+Helix::Vector4 operator/(const float lhs, const Helix::Vector4 &rhs);
+
 #endif // HVECTOR_H
 
