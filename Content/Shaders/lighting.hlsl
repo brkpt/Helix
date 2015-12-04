4736b99c0f6ddb93af1bd1c3b942e356aa45b707
Matt Smith <boulderfans@comcast.net>
2015-12-04 13:08:04 -0700

    Whoops.  We want to keep these constants in a shared file.
    Still have the gbuffer shader that uses them.

--- Content/Shaders/lighting.hlsl
+++ Content/Shaders/lighting.hlsl
@@ -3,49 +3,7 @@
 // To compile offline:
 //  fxc /T fx_5_0 /Vi /Zi /Fo texture.fxo texture.fx
 
-// ****************************************************************************
-// Constant buffers
-//
-// Pixel Shader model 5:
-// 15 buffers
-// 1024 constants of 4 elements apiece
-// ****************************************************************************
-cbuffer VSPerFrameConstants : register(b0)
-{
-	matrix		g_mView			; // c0
-	matrix		g_mProjection	; // c4
-	matrix		g_mInvView		; // c8
-	matrix		g_mView3x3		; // c12
-	matrix		g_mInvViewProj	; // c16
-	matrix		g_mInvProj		; // c20
-	float		viewAspect		; // c24
-}
-
-cbuffer VSPerObjectConstants : register(b1)
-{
-	matrix		g_mWorldView		; // c0
-	matrix		g_mViewWorldIT		; // c4
-	matrix		g_mInvWorldViewProj	; // c8
-};
-
-cbuffer PSPerFrameConstants : register(b2)
-{
-	float4	g_sunColor		; // c0
-	float4	g_sunDir		; // c1
-	float4	g_ambientColor	; // c2;
-	float	g_cameraNear	; // c3;
-	float	g_cameraFar		; // c3;
-	float	g_imageWidth	; // c3;
-	float	g_imageHeight	; // c3;
-	float	g_invTanHalfFOV	; // c4;
-};
-
-cbuffer PSPointLight : register(b3)
-{
-	float4	pointLoc		;
-	float4	pointColor		;
-	float	lightRadius		;
-};
+#include "shared.hlsl"
 
 // ****************************************************************************
 // Vertex/pixel definitions
